13b03f550c92aa10f4994d18f37d0883200fc738
onepercentout@gmail.com <onepercentout@gmail.com>
2015-01-02 18:34:52 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var _ = require('underscore');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+var Client = require('node-rest-client').Client;
+
+client = new Client();
+
+var nodes = { };
+var usernames = {};
+server.listen(process.env.PORT || 3000);
+
+app.set('view engine', 'ejs');
+app.set('view options', { layout: false });
+app.use(express.methodOverride());
+app.use(express.bodyParser());  
+app.use(app.router);
+app.use('/public', express.static('public'));
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+app.get('/about', function(req, res) {
+    
+    client.get("http://ip.jsontest.com/", function(data, response){
+        var jsonData = JSON.parse(data);
+
+        res.render('about',{
+                data: jsonData,
+                response: response
+             });
+     });
+});
+
+io.sockets.on('connection', function(socket) {
+	socket.on('sendchat', function (data) {
+		io.sockets.emit('updatechat', socket.username, data);
+	});
+
+	socket.on('adduser', function(username) {
+		socket.username = username;
+
+		usernames[username] = username;
+
+		socket.emit('servernotification', { connected: true, to_self: true, username: username });
+
+		socket.broadcast.emit('servernotification', { connected: true, username: username });
+
+		io.sockets.emit('updateusers', usernames);
+	});
+
+	// when the user disconnects.. perform this
+	socket.on('disconnect', function(){
+
+		delete usernames[socket.username];
+
+		io.sockets.emit('updateusers', usernames);
+
+		socket.broadcast.emit('servernotification', { username: socket.username });
+	});
+});
